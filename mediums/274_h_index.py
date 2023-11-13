6e699cd711636d00ab3ead96d23b9d1fad2f0cba
Arnav Kohli <arnavkohli2003@gmail.com>
2023-11-13 18:28:24 +0000

    Add solution for 274
    Added solution for problem 274, h indices. Unable to get the
    O(nlogn) solution, had to settle with the O(n^2). Look at
    explanation for details.
    
    Signed-off-by: Arnav Kohli <arnavkohli2003@gmail.com>

--- mediums/274_h_index.py
+++ mediums/274_h_index.py
@@ -23,29 +23,55 @@
 # Example 3:
 # Input: citations = [1]
 # Output: 1
+
+# O(nlogn) solution:
+# To compute the researcher's h-index, we can sort the citations array in
+# non-increasing order and then iterate through the sorted array. For each
+# citation count, we compare it to the number of papers that have at least
+# that many citations, which can be calculated as the remaining elements in
+# the array. If the citation count is greater than or equal to the number of
+# papers with at least that many citations, we have found the h-index.
+
+# For example, given the citations array [3,0,6,1,5], we can sort it to obtain
+# [6,5,3,1,0]. We then iterate through the sorted array and for each citation
+# count, we compare it to the number of remaining elements in the array. For
+# the first element, 6, there are 5 remaining elements in the array, all of
+# which have at least 6 citations, so the h-index is 6. For the second
+# element, 5, there are 4 remaining elements in the array, all of which have
+# at least 5 citations, so the h-index is 5. For the third element, 3, there
+# are 3 remaining elements in the array that have at least 3 citations, so the
+# h-index is 3.
+
+# Time Complexity: O(nlogn), where n is the length of the citations array.
+# This is because we need to sort the array in non-increasing order, which
+# takes O(nlogn) time.
+
+# Space Complexity: O(1), as we are sorting the array in-place and using only
+# constant extra space.
+
 from typing import List
 
 
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        citations.sort()
-        # [0, 1, 3, 5, 6]
+        citations.sort(reverse=True) # Sort the array in non-increasing order
         citations_length = len(citations)
-        # You need the hIndex for 0 as an initializer - all the elements are
-        # greater than or equal to 0 according to the constraints
-        hIndex = len(hIndex)
-        for i in range(citations_length):
-            # 1 article with at least 1 citation, 2 with 2 and so on...
-            # So, go through the array till you reach a number greater than i
-            ptr = 0
-            while citations[ptr] < i:  # You want to stop at 3 so < not <=
-                ptr += 1  # Sorting allows you to do this
-            # Only change hIndex if it actually satisfies the condition
-            hIndex = i if i <= citations_length - ptr else hIndex
+        hIndex = 0
+
+        # Iterate through the sorted array and compare each citation count to
+        # the number of papers that have at least that many citations
+        for index, element in enumerate(citations):
+            if element >= index+1:
+                # If the citation count is greater than or equal to the number
+                # of papers with at least that many citations, we have found
+                # the h-index
+                hIndex = index+1
 
         return hIndex
 
+
 solution = Solution()
-print(solution.hIndex([1]))
-# assert solution.hIndex([3,0,6,1,5]) == 3
-# assert solution.hIndex([1,3,1]) == 1
+assert solution.hIndex([3,0,6,1,5]) == 3
+assert solution.hIndex([1,3,1]) == 1
+assert solution.hIndex([1]) == 1
+assert solution.hIndex([0]) == 0
